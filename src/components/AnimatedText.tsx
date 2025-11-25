f3a7e672039db63f85e5f78061dcc1f5dc31c013
finaky <finaky228@gmail.com>
2025-11-25 12:50:09 +0100

    feat(components): new animated text utility component

---
+++ src/components/AnimatedText.tsx
@@ -0,0 +1,39 @@
+import { motion } from "framer-motion";
+
+interface IAnimatedText {
+  text: string;
+  className: string;
+  staggerChildren: number;
+}
+
+export const AnimatedText = ({
+  text,
+  className,
+  staggerChildren,
+}: IAnimatedText) => {
+  const letters = Array.from(text);
+
+  const letterAnimation = {
+    hidden: { opacity: 0, y: 5 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  return (
+    <motion.small
+      className={className}
+      initial="hidden"
+      animate="visible"
+      transition={{ staggerChildren: staggerChildren }}
+    >
+      {letters.map((letter, index) => (
+        <motion.span
+          key={index}
+          variants={letterAnimation}
+          className="inline-block"
+        >
+          {letter === " " ? "\u00A0" : letter}
+        </motion.span>
+      ))}
+    </motion.small>
+  );
+};
