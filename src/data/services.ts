a755c8ada20a53f88ad601fc10ff2e5d2aee82cf
finaky <finaky228@gmail.com>
2025-11-27 19:27:08 +0100

    feat: config file for services

---
+++ src/data/services.ts
@@ -0,0 +1,29 @@
+export const services = [
+  {
+    title: "Web Design",
+    description:
+      "Our Web Design service is all about creating visually stunning and user-friendly websites that leave a lasting impression on your audience. We blend creativity with functionality to deliver websites that showcase your brand's identity while ensuring seamless navigation and optimal user experience.",
+    price: "1,500",
+  },
+
+  {
+    title: "Web Development",
+    description:
+      "Our Web Development service is focused on turning your website into a powerful digital asset. We utilize the latest technologies and industry best practices to build dynamic and scalable websites that cater to your business objectives.",
+    price: "1,800",
+  },
+
+  {
+    title: "Mobile App Development",
+    description:
+      "With our Mobile App Development service, we harness the power of mobile technology to create cutting-edge applications that engage your customers on-the-go. Our team Our team of skilled developers builds feature-rich and intuitive apps for both Android and iOS platforms.",
+    price: "2,500",
+  },
+
+  {
+    title: "Digital Marketing",
+    description:
+      "In the digital age, marketing is a critical aspect of your business's success. Our Digital Marketing service employs data-driven strategies to enhance your brand's visibility, attract targeted traffic, and boost conversions. From SEO and PPC campaigns to social media marketing, we've got your digital marketing needs covered.",
+    price: "1,200",
+  },
+];
